ad7f730e58346c5910010f2f4db8132f4bb96fa6
Asmaa <asmaa.alnablsi@tatweer.sy>
2024-12-17 13:43:15 +0300

    fix bug

--- resources/js/shared/components/t-modal.vue
+++ resources/js/shared/components/t-modal.vue
@@ -1,11 +1,9 @@
 <template>
  <v-dialog persistent v-model="show" :width="width">
-    <t-loader></t-loader>
       <slot></slot>
  </v-dialog>
 </template>
 <script setup>
-import TLoader from "@/shared/components/t-loader.vue"; 
 const show = defineModel("show", {type: Boolean});
 const props = defineProps(["width"]);
 </script>
