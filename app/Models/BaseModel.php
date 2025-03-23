4b03d5332673966a9ce551ca2460efa6ec93601b
Asmaa <asmaa.alnablsi@tatweer.sy>
2025-03-23 14:12:48 +0300

    add has relatoin to BaseModel

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -6,6 +6,7 @@ use App\Traits\BaseTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
 
 class BaseModel extends Model
 {
@@ -17,5 +18,15 @@ class BaseModel extends Model
     public function toggleActivation(): bool
     {
         return $this->update(['is_active' => !$this->is_active]);
+    }
+
+    public function hasRelations($relations): bool
+    {
+        foreach ($relations as $key => $value) {
+            if($this[Str::snake($value) . '_count'])
+            return true;
+        }
+
+        return false;
     }
 }
